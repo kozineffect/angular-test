bb80397b9a379ea3f917b10e8ca5e9f95f21d9b1
Joe Kozlowski <kozineffect@github.com>
2017-09-05 13:02:36 -0400

    Added a simple navbar and bootstrap 4.0.0.

--- test-app/src/app/app.component.ts
+++ test-app/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Welcome to my first Angular App!!!!!';
+  title = 'my Magic the Gathering news and card search page!';
 }
